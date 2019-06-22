e29d563a4ec753d17ed5a4f84546c8e8ca91ce80
Andrey Mishagin <mam_home@mail.ru>
2019-06-22 16:40:32 +0300

    Добавил первичный скелет формы со списком героев

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
+import { FormsModule }   from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HerosAndComicsComponent } from './heros-and-comics/heros-and-comics.component';
@@ -25,7 +27,9 @@ const routes: Routes =[
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/heros-and-comics/heros/heros.component.ts
+++ src/app/heros-and-comics/heros/heros.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-heros',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./heros.component.css']
 })
 export class HerosComponent implements OnInit {
+
+  searchForm: FormGroup = new FormGroup({
+    name: new FormControl('', [])
+  });
 
   constructor() { }
 
