bf712b56a01b9384d4dd62f728a7ebd447bdbe21
Andrey Mishagin <mam_home@mail.ru>
2019-06-22 16:08:39 +0300

    Созадн первичный скелет и первичная навигация

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HerosAndComicsComponent } from './heros-and-comics/heros-and-comics.component';
+import { HerosComponent } from './heros-and-comics/heros/heros.component';
+import { ComicsComponent } from './heros-and-comics/comics/comics.component';
+
+const routes: Routes =[
+  { path: '', component: HerosAndComicsComponent, pathMatch: 'full'},
+  { path: 'heros', component: HerosComponent},
+  { path: 'comics', component: ComicsComponent },
+  { path: '**', redirectTo: ''}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HerosComponent,
+    ComicsComponent,
+    HerosAndComicsComponent,
+    HerosComponent,
+    ComicsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/heros-and-comics/heros-and-comics.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-heros-and-comics',
+  templateUrl: './heros-and-comics.component.html',
+  styleUrls: ['./heros-and-comics.component.css']
+})
+export class HerosAndComicsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
