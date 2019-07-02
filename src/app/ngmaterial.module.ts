5bfb792f0d1c45c9bfb37feb61400e322ecc4411
Andrey Mishagin <mam_home@mail.ru>
2019-07-02 22:35:51 +0300

    Auto-generated commit

--- src/app/ngmaterial.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from '@angular/core';
-import { MatButtonModule, MatIconModule, MatProgressSpinnerModule } from '@angular/material';
-
-@NgModule({
-imports: [MatButtonModule, MatIconModule, MatProgressSpinnerModule],
-exports: [MatButtonModule, MatIconModule, MatProgressSpinnerModule]
-})
-export class MaterialAppModule { }
